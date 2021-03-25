0a78afc9b9d27f118d08c7e5a7b98c357301da24
kradunmaker@gmail.com <kradunmaker@gmail.com>
2021-03-25 19:06:52 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿
+namespace FirstMachineLearningApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.donwloadPictureToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.findingThingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ContextMenuStrip = this.contextMenuStrip1;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 450);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.donwloadPictureToolStripMenuItem,
+            this.findingThingsToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 70);
+            // 
+            // donwloadPictureToolStripMenuItem
+            // 
+            this.donwloadPictureToolStripMenuItem.Name = "donwloadPictureToolStripMenuItem";
+            this.donwloadPictureToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.donwloadPictureToolStripMenuItem.Text = "Donwload picture";
+            this.donwloadPictureToolStripMenuItem.Click += new System.EventHandler(this.donwloadPictureToolStripMenuItem_Click);
+            // 
+            // findingThingsToolStripMenuItem
+            // 
+            this.findingThingsToolStripMenuItem.Name = "findingThingsToolStripMenuItem";
+            this.findingThingsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.findingThingsToolStripMenuItem.Text = "Finding things";
+            this.findingThingsToolStripMenuItem.Click += new System.EventHandler(this.findingThingsToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem donwloadPictureToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem findingThingsToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,72 @@
+﻿using Alturos.Yolo;
+using Alturos.Yolo.Model;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FirstMachineLearningApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string ImageFileName = string.Empty;
+        Image<Bgr,byte> ImageInput = null;
+        private void donwloadPictureToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFile = new OpenFileDialog())
+            {
+                if (openFile.ShowDialog()==DialogResult.OK)
+                {
+                    ImageFileName = openFile.FileName;
+                    pictureBox1.Image = Image.FromFile(ImageFileName);
+                }
+            }
+        }
+
+        private void findingThingsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var configurationDetector = new ConfigurationDetector();
+            var config = configurationDetector.Detect();
+            using (var yoloWraper = new YoloWrapper(config))
+            {
+                var items = yoloWraper.Detect(ImagetoByte(pictureBox1.Image));
+                if (items.Count()>0)
+                {
+                    foreach (YoloItem item in items)
+                    {
+                        Bitmap bitmap = new Bitmap(pictureBox1.Image);
+                        Rectangle rectangle = new Rectangle(item.X,item.Y,item.Width,item.Height);
+                        ImageInput = new Image<Bgr, byte>(bitmap);
+                        ImageInput.Draw(rectangle, new Bgr(0, 255, 0),3);
+                        ImageInput.Draw($"{item.Type} ({string.Format( "{0:0.0}",item.Confidence)})",
+                            new Point(item.X-3,item.Y-3),Emgu.CV.CvEnum.FontFace.HersheyComplex,0.5,
+                            new Bgr(Color.DarkBlue) );
+                        pictureBox1.Image = ImageInput.Bitmap;
+                    }
+                }
+            }
+        }
+        private byte[] ImagetoByte(Image image)
+        {
+            using (var stream =new MemoryStream())
+            {
+                image.Save(stream,ImageFormat.Jpeg);
+                return stream.ToArray();
+            }
+           
+        }
+    }
+}
